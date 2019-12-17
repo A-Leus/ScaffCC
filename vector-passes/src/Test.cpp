2405bb64efd53945c14e79c3200dcdedf731f39c
Checkmate50 <dgeisler50@gmail.com>
2019-12-17 01:00:59 -0700

    Updates to the pass and tests

--- vector-passes/src/Test.cpp
+++ vector-passes/src/Test.cpp
@@ -1,5 +1,8 @@
 #include "llvm/Pass.h"
-#include "llvm/Function.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Type.h"
+#include "llvm/IR/InstrTypes.h"
+#include "llvm/IR/IRBuilder.h"
 #include "llvm/Support/raw_ostream.h"
 
 #include <stdio.h>
@@ -16,10 +19,36 @@ namespace {
     TestPass() : FunctionPass(ID) {}
 
     virtual bool runOnFunction(Function &F) {
+      LLVMContext &Ctx = F.getContext();
 
       errs() << "run on func\n";
       // ret whether code was modified or not
-      return false;
+      
+      bool modified = false;
+      for (auto& B : F) {
+        for (auto& I : B) {
+          auto* op = &I;
+          IRBuilder<> builder(op);
+          builder.SetInsertPoint(&B, ++builder.GetInsertPoint());
+          Value* newOp = NULL;
+          std::vector<Value*> opers;
+          // newOp = op->clone();
+          // op->setOperand(0);
+          for (int i = 0; i < op->getNumOperands(); i++) {
+            errs() << op->getOperand(i)->getName();
+          }
+          errs() << ";" << op->getOpcodeName() << "--" << op->getType()->isPointerTy() << "\n";
+          // errs() << op->getNumUses() << "--\n";
+          if (!newOp)
+            continue;
+          for (auto& U : op->uses()) {
+            User* user = U.getUser();  // A User is anything with operands.
+            user->setOperand(U.getOperandNo(), newOp);
+          }
+          modified = true;
+        }
+      }
+      return modified;
     }
   };
 }
