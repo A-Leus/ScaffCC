540531a4d54402b146233f2e3755da3768e4f70e
Checkmate50 <dgeisler50@gmail.com>
2019-12-18 00:52:12 -0700

    Basic vectorization working

--- vector-passes/src/Test.cpp
+++ vector-passes/src/Test.cpp
@@ -1,8 +1,8 @@
 #include "llvm/Pass.h"
-#include "llvm/IR/Function.h"
-#include "llvm/IR/Type.h"
-#include "llvm/IR/InstrTypes.h"
-#include "llvm/IR/IRBuilder.h"
+#include "llvm/Function.h"
+#include "llvm/Type.h"
+#include "llvm/InstrTypes.h"
+#include "llvm/Support/IRBuilder.h"
 #include "llvm/Support/raw_ostream.h"
 
 #include <stdio.h>
@@ -11,6 +11,8 @@
 // https://stackoverflow.com/questions/13626993/is-it-possible-to-add-arguments-for-user-defined-passes-in-llvm
 
 using namespace llvm;
+
+const int REP_COUNT = 3;
 
 namespace {
   // TODO potentially want a LoopPass b/c want to trace across within a loop nest and not outside
@@ -25,26 +27,60 @@ namespace {
       // ret whether code was modified or not
       
       bool modified = false;
+      int skip = 0;
       for (auto& B : F) {
         for (auto& I : B) {
+          if (skip > 0) {
+            skip--;
+            continue;
+          }
           auto* op = &I;
           IRBuilder<> builder(op);
           builder.SetInsertPoint(&B, ++builder.GetInsertPoint());
-          Value* newOp = NULL;
-          std::vector<Value*> opers;
-          // newOp = op->clone();
-          // op->setOperand(0);
-          for (int i = 0; i < op->getNumOperands(); i++) {
-            errs() << op->getOperand(i)->getName();
-          }
+          std::vector<Value*> newOps;
+          Value* newOp;
           errs() << ";" << op->getOpcodeName() << "--" << op->getType()->isPointerTy() << "\n";
-          // errs() << op->getNumUses() << "--\n";
-          if (!newOp)
-            continue;
-          for (auto& U : op->uses()) {
-            User* user = U.getUser();  // A User is anything with operands.
-            user->setOperand(U.getOperandNo(), newOp);
+
+          std::string op_code = op->getOpcodeName();
+          if (op_code == "alloca") { // Only one operand
+            AllocaInst* alloca_op = (AllocaInst*) op;
+            Type* alloca_type = alloca_op->getAllocatedType();
+            Value* alloca_size = alloca_op->getArraySize();
+            for (int i = 0; i < REP_COUNT - 1; i++) {
+              Value* newAlloca = builder.CreateAlloca(alloca_type, alloca_size);
+              Value* newCpy = builder.CreateMemCpy(newAlloca, alloca_op, alloca_size, 2);
+              newOps.push_back(newAlloca);
+            }
+            skip = REP_COUNT * 3;
           }
+          
+          // errs() << op->getNumUses() << "--\n";
+          if (newOps.size() == 0)
+            continue;
+          
+          auto next_op = op;
+          // I can't figure out the recursion here tonight...just too tired.
+          for (int i = 0; i < 1; i++) {
+          // while (next_op->getNumUses() > 0) {
+            std::vector<Value*> nextOps;
+            for (auto iter = next_op->use_begin(); !iter.atEnd(); iter++) {
+              User* cur_user = *iter;
+              next_op = (Instruction*) cur_user;
+              for (int i = 0; i < REP_COUNT - 1; i++) {
+                auto new_instr = next_op->clone();
+                new_instr = builder.Insert(new_instr);
+                nextOps.push_back(new_instr);
+                for (int j = 0; j < cur_user->getNumOperands(); j++)
+                  if (cur_user->getOperand(j) == op)
+                    new_instr->setOperand(j, newOps[i]);
+              }
+            }
+            newOps = nextOps;
+          }
+          // for (auto& U : op->uses()) {
+          //   User* user = U.getUser();  // A User is anything with operands.
+          //   user->setOperand(U.getOperandNo(), newOp);
+          // }
           modified = true;
         }
       }
